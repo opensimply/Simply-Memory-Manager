b5e09c3ab3de122f873c538288f3dfd63d7b33de
Free simulation software <26973313+opensimply@users.noreply.github.com>
2023-03-05 09:47:55 +0100

    Add files via upload

---
+++ smmusage.pas
@@ -0,0 +1,17 @@
+(******************************************************************************
+
+   Open source free memory manager for Free Pascal amd Lazarus on Windows.
+
+   Download at: https://opensimply.org/smm/ 
+
+*******************************************************************************)
+
+
+program SMMUsage;
+uses
+  SimplyMM;
+
+begin
+  WriteLn('Simply Memory Manager ',GetVersionAsStr);
+end.
+
